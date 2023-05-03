728af56f65531bfea9420c9477e7b5bbcc4f4e52
Ahmed Aziz Smaoui <42848675+loading04@users.noreply.github.com>
2023-05-03 13:31:05 +0100

    adding data (Ariana Ville)
    adding all the city in ariana ville with zip codes

---
+++ api/data.py
@@ -0,0 +1,48 @@
+data = {
+    "region1": {
+        "Ariana": {
+            "region2": {
+                "Ariana Ville": {
+                    [
+                        {"Ariana": "2080"},
+                        {"Borj El Baccouche": "2027"},
+                        {"Centre Commercial Ikram": "2037"},
+                        {"Cité Belvedere 2": "2091"},
+                        {"Cité Borj Turki 1": "2058"},
+                        {"Cité Borj Turki 2": "2058"},
+                        {"Cité des Roses": "2080"},
+                        {"Cité du Jardin": "	2080"},
+                        {"Cité du Printemps": "2080"},
+                        {"Cité du Soleil": "2091"},
+                        {"Cité El Intissar 1": "2091"},
+                        {"Cité El Intissar 2": "2091"},
+                        {"Cité Ennasr 1": "2037"},
+                        {"Cité Ennasr 2": "2037"},
+                        {"Cité Ennouzha": "2080"},
+                        {"Cité Essaada (ariana)": "2080"},
+                        {"Cité Essaada (riadh Andalous)": "2058"},
+                        {"Cité Jaafar": "2080"},
+                        {"Cité Jamil": "2091"},
+                        {"Cité Karim": "2058"},
+                        {"Cité Mehrzia 1": "2058"},
+                        {"El Menzah 5": "2091"},
+                        {"El Menzah 6": "2091"},
+                        {"El Menzah 7": "2037"},
+                        {"El Menzah 8": "2037"},
+                        {"Nouvelle Ariana": "2080"},
+                        {"Résidence El Ouns": "2058"},
+                        {"Résidence Ennour ( Naser 1)": "2037"},
+                        {"Résidence Ennour ( Naser 2)": "2037"},
+                        {"Résidence Ennour (ariana)": "2080"},
+                        {"Résidence Ibn Zeidoun": "2058"},
+                        {"Résidence Ichbilia": "2058"},
+                        {"Résidence Kortoba": "2058"},
+                        {"Riadh Landlous": "2058"}
+                    ]
+
+                }
+            }
+        }
+
+    }
+}
---
+++ api/main.py
@@ -0,0 +1,14 @@
+import random
+
+import fastapi
+
+app = fastapi.FastAPI()
+
+
+
+
+@app.get("/generate_name")
+async def generate_name():
+    names = ["Minnie", "Margaret", "Myrtle", "Noa", "Nadia"]
+    random_name = random.choice(names)
+    return {"name": random_name}
